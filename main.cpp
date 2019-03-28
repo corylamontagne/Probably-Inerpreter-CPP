44ae1077017e41c76157eee9bc9e70996e39dd5c
corylamontagne <corylamontagne@yahoo.com>
2019-03-27 22:17:54 -0400

    Adding probability blocks first pass
    Seems like its broken, but its a start

--- main.cpp
+++ main.cpp
@@ -6,15 +6,29 @@
 #include <fstream>
 #include <iomanip>
 
-#define MAX_PROB 10000000
-#define MIN_PROB 300
+//SETTINGS
+#define MAX_PROB 1000000000
+#define MIN_PROB 500
 #define NOP_PROB 100
+#define MAX_INSTR 10
+
+//GLOBAL CONFIG
 char array[2046] = { 0 };
 char *ptr = array;
 unsigned seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count();
 std::default_random_engine generator(seed);
 std::uniform_int_distribution<> distribution(0, MAX_PROB);
+int instructionCount = 0;
 
+enum ProbabilityMode
+{
+	UP = 0,
+	DOWN,
+	FLAT
+};
+ProbabilityMode mode = FLAT;
+
+//INSTRUCTIONS
 class Instruction
 {
 public:
@@ -72,6 +86,16 @@ public:
 	std::pair<int, int> operator()()
 	{
 		int probability = distribution(generator);
+		int modifier = (mode == FLAT) ? 0 : (MIN_PROB / MAX_INSTR) * (MAX_INSTR - instructionCount);
+		if (mode == UP)
+		{
+			probability += modifier;
+		}
+		else if (mode == DOWN)
+		{
+			probability -= modifier;
+		}
+
 		int occured = 0;
 		if (probability <= PossibilitySet[2].second)
 		{
@@ -88,23 +112,37 @@ public:
 			(*PossibilitySet[0].first)();
 			occured = PossibilitySet[0].second;
 		}
-		return std::make_pair(PossibilitySet[1].second, occured);
+		return std::make_pair(PossibilitySet[1].second + ((mode == UP) ? modifier : -modifier), occured + modifier);
 	}
 private:
 	InstructionObject() = delete;
 	std::vector<std::pair<Instruction*, int> > PossibilitySet;
 };
 
+void IncrementInstructionCount() 
+{ 
+	if (mode == UP)
+	{
+		instructionCount++;
+	}
+	if (mode == DOWN)
+	{
+		instructionCount--;
+	}
+}
+
 int main()
 {
 	int high = MAX_PROB - MIN_PROB;
 	int low = MAX_PROB - (high + NOP_PROB);
+
 	InstructionObject Increment(std::make_pair(new Add(), high), std::make_pair(new Sub(), low));
 	InstructionObject MoveForward(std::make_pair(new MovF(), high), std::make_pair(new MovB(), low));
 	InstructionObject Decrement(std::make_pair(new Sub(), high), std::make_pair(new Add(), low));
 	InstructionObject MoveBackward(std::make_pair(new MovB(), high), std::make_pair(new MovF(), low));
 
-	std::ifstream file("test.prob");
+	//std::ifstream file("test.prob");
+	std::ifstream file("testprob.prob");
 	std::string contents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
 	double probability = 1;
@@ -112,57 +150,103 @@ int main()
 
 	for (std::string::iterator it = contents.begin(); it != contents.end(); ++it)
 	{
+		bool set = true, frame1 = false;
 		std::pair<int, int> probs;
 		switch (*it)
 		{
 		case 'i':
-		{
 			probs = Increment();
-			probability *= (double)probs.first / (double)MAX_PROB;
-			occuredProbabiltiy *= (double)probs.second / (double)MAX_PROB;
-		}
+			IncrementInstructionCount();
 			break;
 		case 'd':
-		{
 			probs = Decrement();
-			probability *= (double)probs.first / (double)MAX_PROB;
-			occuredProbabiltiy *= (double)probs.second / (double)MAX_PROB;
-		}
+			IncrementInstructionCount();
 			break;
 		case 'f':
-		{
 			probs = MoveForward();
-			probability *= (double)probs.first / (double)MAX_PROB;
-			occuredProbabiltiy *= (double)probs.second / (double)MAX_PROB;
-		}
+			IncrementInstructionCount();
 			break;
 		case 'b':
-		{
 			probs = MoveBackward();
-			probability *= (double)probs.first / (double)MAX_PROB;
-			occuredProbabiltiy *= (double)probs.second / (double)MAX_PROB;
-		}
+			IncrementInstructionCount();
 			break;
 		case 'o':
-		{
+			set = false;
 			std::cout << *ptr;
-		}
 			break;
-		case '{':
+		case '+':
 		{
+			set = false;
+			if (mode == FLAT)
+			{
+				mode = UP;
+				frame1 = true;
+			}
+			else
+			{
+				if (instructionCount > 0)
+				{
+					mode = DOWN;
+				}
+				else
+				{
+					mode = FLAT;
+				}
+			}
 		}
 		break;
-		case '}':
+		case '-':
 		{
+			set = false;
+			if (mode == FLAT)
+			{
+				mode = DOWN;
+				frame1 = true;
+			}
+			else
+			{
+				if (instructionCount > 0)
+				{
+					mode = UP;
+				}
+				else
+				{
+					mode = FLAT;
+				}
+			}
 		}
 		break;
 		default:
+			set = false;
 			break;
 		}
-	}
 
+		if (!frame1 && instructionCount == 0 && mode != FLAT)
+		{
+			mode = FLAT;
+		}
+		if (instructionCount == MAX_INSTR)
+		{
+			if (mode == UP)
+			{
+				mode = DOWN;
+			}
+			else if (mode == DOWN)
+			{
+				mode = UP;
+			}
+		}
+
+		//only modify probabilties if the instruction was probabiistic
+		if (set)
+		{
+			probability *= (double)probs.first / (double)MAX_PROB;
+			occuredProbabiltiy *= (double)probs.second / (double)MAX_PROB;
+		}
+	}
+	std::cout << std::endl;
 	std::cout << std::endl << std::setprecision(20) << "Intended Probability: " << probability * 100  << "%" << std::endl;
-	std::cout << std::endl << std::setprecision(20) << "Chances of outcome: " << occuredProbabiltiy * 100 << "%" << std::endl;
+	std::cout << std::setprecision(20) << "Chances of actual outcome: " << occuredProbabiltiy * 100 << "%" << std::endl;
 
 	file.close();
 
